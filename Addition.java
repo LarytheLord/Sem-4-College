8b9e039f2c8ba0120e0ba2ceb6c7b77a6bba70db
Abi <llawlietbagsum@gmail.com>
2026-01-06 12:35:24 +0530

    Class 1 Oop

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+	public static void main(String [] args)
+	{		
+		int a = 100;
+		int b = 225;
+		int sum = a + b;
+		System.out.println("225 + 100 = "+sum);
+	}
+}
---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello
+{
+	public static void main(String [] args)
+	{		
+		System.out.println("Hello, World");
+		System.out.println("20 minus 10 is equal  "+(20-10));
+	}
+}
